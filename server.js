e4e42977e54b4fd58f66ab6d78873ce6acbf3c10
Rahul1309200 <146360893+Rahul1309200@users.noreply.github.com>
2025-03-03 10:24:58 +0530

    all the third party  middlewares added

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ app.use(
 const cors = require('cors');
 app.use(cors()); // Enable all origins (for APIs)
 
+const bodyParser=require('body-parser')
+app.use(bodyParser.json());
+// Middleware to parse URL-encoded data (from HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const rateLimit = require('express-rate-limit'); // Import the package
+
+// Define a rate limit: Allow 100 requests per 15 minutes per IP
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.',
+    headers: true, // Send rate limit info in headers
+});
+
+
+// Apply the rate limiter to all API routes
+app.use('/api', limiter);
+
 
 const PORT = 8080
 // Import middlewares
